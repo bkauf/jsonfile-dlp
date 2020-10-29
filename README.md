80843a14e15791f76df6a8c298de30f23524e513
Brian Kaufman <bkauf@google.com>
2020-10-29 21:07:09 +0000

    file blog support, much faster!

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-### Python JSON file DLP redaction with Google Cloud DLP API
+### Python text file load to  DLP redaction with Google Cloud DLP API
 #### First argument for redaction.py specifies input file, output will append -output.json in new file
-``` python3 redaction.py [json file]```
+``` python3 redaction-blob.py [json file]```
 #### Combine tabs or double spaces to single spaces with this command before running script
 ```tr -s '[:blank:]' ' ' <file >newfile```
-#### Split file into multiple 100mb smaller ones
+#### Split file into multiple 100mb smaller ones if nessessary
 ```split -b 100MB input.json```
 
 #### Run Script in the background
 
-```nohup python3 redaction.py [jsonfile] &``` 
+```nohup python3 redaction-blob.py [jsonfile] &``` 
---
+++ redaction-blob.py
@@ -0,0 +1,74 @@
+import ijson
+import sys
+import json
+#from google.cloud import dlp_v2
+# Import the client library
+import google.cloud.dlp
+
+key_path = './sa-token.json'
+inputfile = sys.argv[1]
+outputfile = inputfile+'-output.json'
+catches = 'catches.csv'
+
+
+dlp_client = google.cloud.dlp_v2.DlpServiceClient.from_service_account_file(key_path)
+
+#info_types = ["PERSON_NAME"]
+
+deidentify_template_name = "projects/bkauf-sandbox/locations/global/deidentifyTemplates/sample-aggressive1"
+inspect_template_name = "projects/bkauf-sandbox/locations/global/inspectTemplates/sample-aggressive1"
+
+min_likelihood = google.cloud.dlp_v2.Likelihood.LIKELIHOOD_UNSPECIFIED
+inspect_config = {
+    #"info_types": [{"name": info_type} for info_type in info_types],
+    #"min_likelihood": min_likelihood
+}
+replacement_str= "PERSON_NAME"
+# Construct deidentify configuration dictionary
+deidentify_config = {
+    "info_type_transformations": {
+        "transformations": [
+            {
+                "primitive_transformation": {
+                    "replace_config": {
+                        "new_value": {"string_value": replacement_str}
+                    }
+                }
+            }
+        ]
+    }
+}
+
+
+# Convert the project id into a full resource id.
+parent = f"projects/bkauf-sandbox"
+
+with open(inputfile) as f:
+    data = f.read(250000)
+    while data !=b"":
+    
+        
+        item = {"value": data} 
+                
+        response = dlp_client.deidentify_content(
+            request={
+                "parent": parent,
+                #"deidentify_config": deidentify_config,
+                "inspect_template_name": inspect_template_name,
+                "inspect_config": inspect_config,
+                "deidentify_template_name": deidentify_template_name,
+                "item": item,
+            }
+        )
+
+                
+        dataDLP = response.item.value
+        # Print out the results if there are matches
+                
+        # Open a file with access mode 'a'
+        file_object = open(outputfile, 'a')
+        # Append 'hello' at the end of file
+        file_object.write(dataDLP)
+        # Close the file
+        file_object.close()
+        data = f.read(250000)
